e5f1288b0680f652910879ee843bbb2feeeb4b3e
Mridul Seth <seth.mridul@gmail.com>
2020-06-10 04:57:55 +0530

    blackify codebase

--- nbreproduce/cli.py
+++ nbreproduce/cli.py
@@ -2,7 +2,13 @@
 import argparse
 import sys
 
-from .nbreproduce import download_notebook_from_url, check_docker_image, reproduce, link_docker_notebook
+from .nbreproduce import (
+    download_notebook_from_url,
+    check_docker_image,
+    reproduce,
+    link_docker_notebook,
+)
+
 
 def main():
     """Console script for nbreproduce."""
@@ -11,33 +17,31 @@ def main():
     parser.add_argument(
         "--url", help="URL to notebook, currently works only for GitHub"
     )
-    parser.add_argument(
-        "--docker", help="Name of Docker image on DockerHub"
-    )
-    parser.add_argument(
-        "--timeout", help="indvidual cell timeout limit, default 600s"
-    )
+    parser.add_argument("--docker", help="Name of Docker image on DockerHub")
+    parser.add_argument("--timeout", help="indvidual cell timeout limit, default 600s")
     # parser.add_argument('_', nargs='*')
     args = parser.parse_args()
     print(args)
     if args.url is not None:
-        print(f'Downloading Jupyter Notebook from the provided URL: {args.url}')
+        print(f"Downloading Jupyter Notebook from the provided URL: {args.url}")
         notebook = download_notebook_from_url(args.url)
-        print('Download successful')
+        print("Download successful")
     if args.notebook is not None:
         # sanity check, notebook extension
-        if args.notebook[-6:] != '.ipynb':
-            raise ValueError('Not a Jupyter notebook')
-        if args.notebook[:4] == 'http':
-            raise ValueError('Use --url flag to pass in a URL to a Jupyter Notebook')
+        if args.notebook[-6:] != ".ipynb":
+            raise ValueError("Not a Jupyter notebook")
+        if args.notebook[:4] == "http":
+            raise ValueError("Use --url flag to pass in a URL to a Jupyter Notebook")
         notebook = args.notebook
 
     if not check_docker_image(notebook):
         if args.docker is None:
-            raise ValueError(f'No linked Docker image found in the metadata, link it using "nbreproduce --docker image_name {notebook}" \
-                The Docker image should be built using the base Jupyter docker-stacks images, https://jupyter-docker-stacks.readthedocs.io')
+            raise ValueError(
+                f'No linked Docker image found in the metadata, link it using "nbreproduce --docker image_name {notebook}" \
+                The Docker image should be built using the base Jupyter docker-stacks images, https://jupyter-docker-stacks.readthedocs.io'
+            )
         link_docker_notebook(notebook, args.docker)
-    
+
     # force new docker link
     if args.docker is not None:
         link_docker_notebook(notebook, args.docker)
--- nbreproduce/nbreproduce.py
+++ nbreproduce/nbreproduce.py
@@ -5,23 +5,24 @@ import subprocess
 
 NB_VERSION = 4
 
+
 def download_notebook_from_url(url: str) -> str:
     # convert GitHub content page URL to raw URL
-    if url.startswith('https://github.com'):
-        url = url.replace('https://github.com', 'https://raw.githubusercontent.com')
-        url = url.replace('/blob', '')
-    elif url.startswith('https://raw.githubusercontent.com'):
+    if url.startswith("https://github.com"):
+        url = url.replace("https://github.com", "https://raw.githubusercontent.com")
+        url = url.replace("/blob", "")
+    elif url.startswith("https://raw.githubusercontent.com"):
         pass
     else:
-        raise ValueError('URL not valid')
-    
+        raise ValueError("URL not valid")
+
     # check ipynb extension
-    if url[-6:] != '.ipynb':
-        raise ValueError('Not a Jupyter notebook')
-    FILE_NAME = url.split('/')[-1]
-    with urllib.request.urlopen(url) as response, open(FILE_NAME, 'wb') as out_file:
+    if url[-6:] != ".ipynb":
+        raise ValueError("Not a Jupyter notebook")
+    FILE_NAME = url.split("/")[-1]
+    with urllib.request.urlopen(url) as response, open(FILE_NAME, "wb") as out_file:
         shutil.copyfileobj(response, out_file)
-    
+
     return FILE_NAME
 
 
@@ -29,32 +30,37 @@ def check_docker_image(notebook):
     # check for notebook existence?
     nb = nbformat.read(notebook, as_version=NB_VERSION)
     # check if docker_image is used previously
-    if 'docker_image' not in nb['metadata']:
-        print('No linked Docker image found!')
+    if "docker_image" not in nb["metadata"]:
+        print("No linked Docker image found!")
         return False
     return True
 
+
 def link_docker_notebook(notebook, docker):
     nb = nbformat.read(notebook, as_version=NB_VERSION)
-    nb['metadata']['docker_image'] = docker
+    nb["metadata"]["docker_image"] = docker
     nbformat.write(nb, notebook)
 
 
 def reproduce(notebook, timeout):
     nb = nbformat.read(notebook, as_version=NB_VERSION)
-    DOCKER_IMAGE = nb['metadata']['docker_image']
+    DOCKER_IMAGE = nb["metadata"]["docker_image"]
     # check docker link
     print(DOCKER_IMAGE)
     NOTEBOOK_NAME = notebook[:-6]
     pwd = subprocess.run(["pwd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     mount = str(pwd.stdout)[2:-3] + ":/home/jovyan/work"
     # mount the present directory and start up a container
-    print('Fetching the docker copy for reproducing results, this may take some time if running the reproduce.py command for the first time')
+    print(
+        "Fetching the docker copy for reproducing results, this may take some time if running the reproduce.py command for the first time"
+    )
     container_id = subprocess.run(
-        ["docker", "run", "-v", mount, "-d", DOCKER_IMAGE], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        ["docker", "run", "-v", mount, "-d", DOCKER_IMAGE],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
     container_id = container_id.stdout.decode("utf-8")[:-1]
-    print(f'A docker container is created to execute the notebook, id = {container_id}')
+    print(f"A docker container is created to execute the notebook, id = {container_id}")
     PATH_TO_NOTEBOOK = f"/home/jovyan/work/"
     # copy the notebook file to reproduce notebook
     subprocess.run(
@@ -77,4 +83,3 @@ def reproduce(notebook, timeout):
     #     shell=True,
     # )
     subprocess.run([f"docker stop {container_id}"], shell=True)
-  
