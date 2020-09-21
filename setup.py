e6e334ef37147974e568cf07eabb9f9878f5a88e
Mridul Seth <seth.mridul@gmail.com>
2020-09-22 01:48:50 +0530

    bump up to 0.4.0

--- setup.py
+++ setup.py
@@ -19,15 +19,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Mridul Seth",
     author_email='seth.mridul@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
@@ -49,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MridulS/nbreproduce',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )
