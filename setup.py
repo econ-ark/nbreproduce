974b2cda823c82c5593724702108e9062f3c11fb
Mridul Seth <seth.mridul+github@gmail.com>
2020-09-22 02:01:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
